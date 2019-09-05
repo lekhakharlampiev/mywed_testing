a1c44528801507121c0a0372b511dd37909ec337
Aleksey <kharlampiev@aspect-systems.ru>
2019-09-05 15:21:58 +0300

    Доделал хедер, начал основное содержание

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,21 @@
 'use strict';
 
 const gulp = require('gulp');
-const less = require('gulp-less');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+gulp.task('assets', function() {
+    return gulp.src('app/**/*.*')
+        .pipe(gulp.dest('dist'));
+});
+gulp.task('clean', function() {
+    return del('dist');
+});
+gulp.task('build', gulp.series('clean', 'assets'));
+gulp.task('serve', function() {
+    browserSync.init({
+        server: 'dist'
+    });
+    browserSync.watch('dist/**/*.*').on('change', browserSync.reload);
+});
+
