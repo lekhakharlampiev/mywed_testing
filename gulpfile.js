23c6d675c60aecc3b6b4d6e0571257531c68e13b
Aleksey <xarlamlexa1502@gmail.com>
2019-09-08 20:34:35 +0300

    запуск с команды 'gulp'

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const gulp = require('gulp');
 const del = require('del');
 const browserSync = require('browser-sync').create();
 
+
 gulp.task('assets', function() {
     return gulp.src('app/**/*.*')
         .pipe(gulp.dest('dist'));
@@ -12,10 +13,12 @@ gulp.task('clean', function() {
     return del('dist');
 });
 gulp.task('build', gulp.series('clean', 'assets'));
+
+
 gulp.task('server', function() {
     browserSync.init({
-        server: 'app'
+        server: 'dist'
     });
-    browserSync.watch('app/**/*.*').on('change', browserSync.reload);
 });
 
+gulp.task('default', gulp.series('build', 'server'));
