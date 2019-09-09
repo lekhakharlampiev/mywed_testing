c8aca921e37777cb5e4a28fa3f3e3023d4a64c61
Aleksey <kharlampiev@aspect-systems.ru>
2019-09-09 17:41:04 +0300

    новая сборка

--- app/js/index.js
+++ app/js/index.js
@@ -1,126 +1,3 @@
-let photos_db = [
-    {
-        id: "5c408b7c4465e774e0633f0b",
-        src: "1.jpg",
-        name: "Molina Slater",
-        city: 'Рязань'
-    },
-    {
-        id: "5c408b7cc56e7a6f297b749a",
-        src: "2.jpeg",
-        name: "Molina Slater",
-        city: 'Рязань'
-    },
-    {
-        id: "5c408b7c191eda0f320c96a1",
-        src: "3.jpg",
-        name: "Molina Slater",
-        city: 'Рязань'
-    },
-    {
-        id: "5c408b7cd82408d7ed808aa4",
-        src: "4.jpg",
-        name: "Imelda Jefferson",
-        city: 'Рязань'
-    },
-    {
-        id: "5c408b7cfb53d12428d4613f",
-        src: "5.jpeg",
-        name: "Imelda Jefferson",
-        city: 'Рязань'
-    },
-    {
-        id: "5c408b7cbc28fd1ab8ed729e",
-        src: "6.jpg",
-        name: "Imelda Jefferson",
-        city: 'Москва'
-    },
-    {
-        id: "5c408b7cf92ee8cd9089bbc5",
-        src: "7.jpeg",
-        name: "Imelda Jefferson",
-        city: 'Москва'
-    },
-    {
-        id: "5c408b7c11291ec047504c7d",
-        src: "8.jpg",
-        name: "Imelda Jefferson",
-        city: 'Москва'
-    },
-    {
-        id: "5c408b7ce2e4c0debe0c55fc",
-        src: "9.jpeg",
-        name: "Leslie Mcconnell",
-        city: 'Москва'
-    },
-    {
-        id: "5c408b7cdfbdaf2915371c94",
-        src: "10.jpg",
-        name: "Mandy Kerr",
-        city: 'Москва'
-    },
-    {
-        id: "5c408b7c655975333ee962a6",
-        src: "11.jpeg",
-        name: "Glass Browning",
-        city: 'Москва'
-    },
-    {
-        id: "5c408b7c6a048a4b44e3d964",
-        src: "12.jpg",
-        name: "Maddox Stein",
-        city: 'Москва'
-    },
-    {
-        id: "5c408b7cb9066aa5f7f69089",
-        src: "13.jpg",
-        name: "Webster Lambert",
-        city: 'Москва'
-    },
-    {
-        id: "5c408b7c903a1194f9fea4b1",
-        src: "14.jpg",
-        name: "Loraine Wilkins",
-        city: 'Москва'
-    },
-    {
-        id: "5c408b7c17096cf43dc49e52",
-        src: "15.jpeg",
-        name: "Georgia Graham",
-        city: 'Санкт Петербург'
-    },
-    {
-        id: "5c408b7cc4dceec5db0bc735",
-        src: "16.jpeg",
-        name: "Celina Chavez",
-        city: 'Санкт Петербург'
-    },
-    {
-        id: "5c408b7c7f15eb46b8273c70",
-        src: "17.jpg",
-        name: "Lila Hammond",
-        city: 'Санкт Петербург'
-    },
-    {
-        id: "5c408b7c9a9437c7f42532af",
-        src: "18.jpg",
-        name: "Moran Strong",
-        city: 'Санкт Петербург'
-    },
-    {
-        id: "5c408b7cf42a83cb3794da5b",
-        src: "19.jpg",
-        name: "Moran Strong",
-        city: 'Санкт Петербург'
-    },
-    {
-        id: "5c408b7c2ab826f902b0538d",
-        src: "20.jpg",
-        name: "Moran Strong",
-        city: 'Санкт Петербург'
-    }
-];
-
 const photoContainer = document.querySelector('.js-photos__container');
 
 //рендер всех фото по умолчанию:
@@ -128,7 +5,7 @@ const renderPhotos = (arr) => {
     return arr.map((item) => {
         let img = document.createElement('img');
         img.className = 'js-img-photo';
-        img.src = 'photos/' + item.src;
+        img.src = 'dist/photos/' + item.src;
         let photo = document.createElement('div');
         photo.className = 'photo js-photo';
         let title = document.createElement('h4');
@@ -207,7 +84,7 @@ inputName.addEventListener('input', function() {
             if(item.name.toLowerCase().includes(event.target.value.toLowerCase())) {
                 let img = document.createElement('img');
                 img.className = 'js-img-photo';
-                img.src = 'photos/' + item.src;
+                img.src = 'dist/photos/' + item.src;
                 let photo = document.createElement('div');
                 photo.className = 'photo js-photo';
                 let title = document.createElement('h4');
@@ -238,7 +115,7 @@ inputName.addEventListener('input', function() {
             if(item.name.toLowerCase().includes(event.target.value.toLowerCase()) && item.city.toLowerCase().includes(citySearchInput.value.toLowerCase())) {
                 let img = document.createElement('img');
                 img.className = 'js-img-photo';
-                img.src = 'photos/' + item.src;
+                img.src = 'dist/photos/' + item.src;
                 let photo = document.createElement('div');
                 photo.className = 'photo js-photo';
                 let title = document.createElement('h4');
@@ -280,7 +157,7 @@ const searchByCity = (value) => {
             if(item.city.toLowerCase().includes(value.toLowerCase())) {
                 let img = document.createElement('img');
                 img.className = 'js-img-photo';
-                img.src = 'photos/' + item.src;
+                img.src = 'dist/photos/' + item.src;
                 let photo = document.createElement('div');
                 photo.className = 'photo js-photo';
                 let title = document.createElement('h4');
--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,65 @@
 const gulp = require('gulp');
 const del = require('del');
 const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify-es').default;
 
+const cssFiles = [
+    './app/css/fonts.css',
+    './app/css/normalize.css',
+    './app/css/style.css',
+    './app/css/header.css',
+    './app/css/main.css',
+    './app/css/media_queries.css'
+]
+const jsFiles = [
+    './app/js/data.js',
+    './app/js/index.js'
+] 
 
-gulp.task('assets', function() {
-    return gulp.src('app/**/*.*')
-        .pipe(gulp.dest('dist'));
+gulp.task('styles', function() {
+    return gulp.src(cssFiles)
+    .pipe(concat('style.css'))
+    .pipe(cleanCSS({
+        level: 2
+    }))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream())
+});
+gulp.task('js', function() {
+    return gulp.src(jsFiles)
+    .pipe(concat('script.js'))
+    .pipe(uglify({
+        toplevel: true
+    }))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.stream())
+});
+gulp.task('img', function() {
+    return gulp.src('./app/img/*.*')
+        .pipe(gulp.dest('./dist/img'));
+});
+gulp.task('photos', function() {
+    return gulp.src('./app/photos/*.*')
+        .pipe(gulp.dest('./dist/photos'));
+});
+gulp.task('fonts', function() {
+    return gulp.src('./app/fonts/*.*')
+        .pipe(gulp.dest('./dist/fonts'));
 });
 gulp.task('clean', function() {
-    return del('dist');
+    return del(['dist/*'])
 });
-gulp.task('build', gulp.series('clean', 'assets'));
-
-
 gulp.task('server', function() {
     browserSync.init({
-        server: 'dist'
+        server: {
+            baseDir: './'
+        }
     });
+    //gulp.watch('./app/css/**/*.css', 'styles')
+    //gulp.watch('./app/js/**/*.js', 'js')
+    //gulp.watch('./*.html').on('change', browserSync.reload);
 });
-
+gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'js','img', 'photos', 'fonts') ));
 gulp.task('default', gulp.series('build', 'server'));
